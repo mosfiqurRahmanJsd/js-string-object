7da8bfa686494cb49756168516dd35108066cb5e
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-09-17 21:14:32 +0600

    object

---
+++ object.js
@@ -0,0 +1,25 @@
+const age = 21; 
+const school = 'Rifle Squre'; 
+const isPassed = true; 
+let isDeveloper; 
+
+// const subject = ['Bangla', 'English', 'Physics', 'Chemistry', 'Math']; 
+// console.log(subject); 
+
+// const bottle = {
+//     brand: 'Kinley', 
+//     color: 'White', 
+//     price: 40, 
+//     weight: '2 liter', 
+//     isClean: true
+// }
+
+// object
+const bottle = {
+    brand: 'Apple', 
+    price: 255, 
+    color: 'White', 
+    isClean: false
+}
+
+console.log(bottle); 
