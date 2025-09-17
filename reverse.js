2fb372e0b5a2333a2a091fa1aa61d715b887fbd6
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-09-17 20:35:34 +0600

    reverse string

--- reverse.js
+++ reverse.js
@@ -1,9 +1,25 @@
-const sentence = 'I want to learn web development'; 
+const sentence = 'I want to learn web development';
 
 
-let reverse = ''; 
-for(const letter of sentence) {
-    reverse = letter + reverse; 
+let reverse = '';
+for (const letter of sentence) {
+    // reverse = letter + reverse; 
 }
 
-console.log(reverse); 
+// console.log(reverse); 
+
+
+// let reverseFor = '';
+// for (let i = 0; i < sentence.length; i++) {
+//     const letter = sentence[i]; 
+//     reverseFor = letter + reverseFor;
+// }
+
+// console.log(reverseFor); 
+
+
+// shortcut
+const web = 'I am learning web development'; 
+const rev = web.split('').reverse().join(''); 
+console.log(rev); 
+
