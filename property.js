f11e14d7a038103fe82782eb077d17c540e0c741
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-09-18 21:05:43 +0600

    properties and values

---
+++ property.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Sodor Uddin', 
+    age: 25,
+    proffession: 'developer', 
+    salary: 25000, 
+    married: true, 
+    'fav places': ['bandarban', 'sundarban', 'kuakata'],
+}
+
+const beton = person['salary']; 
+
+console.log(person["fav places"]); 
---
+++ value.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Sodor Uddin', 
+    age: 25,
+    proffession: 'developer', 
+    salary: 25000, 
+    married: true, 
+    'fav places': ['bandarban', 'sundarban', 'kuakata'],
+}
+
+person.salary = 30000;
+person['age'] = 26; 
+person['fav places'] = ['maldivs', 'pataya']; 
+console.log(person); 
